d0cb9e468ea163c3cfddf9d34bac017c8463db0c
Aayshaparveen <aaysha.parveen@ismartapps.co.uk>
2023-05-31 10:53:06 +0530

    Add

--- src/Components/Meal.jsx
+++ src/Components/Meal.jsx
@@ -1,50 +1,55 @@
-import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import MealItem from "./MealItem";
-import {GiFoodTruck} from "react-icons/gi"
+import { GiFoodTruck } from "react-icons/gi";
 // import ReacipeIndex from "./RecipeIndex";
 const Meal = () => {
-    const [search,setSearch]=useState();
-    const [show,setShow]=useState(false);
-    const [item,setItem]=useState("");
-    const [url,setUrl]=useState("https://www.themealdb.com/api/json/v1/1/search.php?f=a");
-   
-    useEffect(()=>{
-        fetch(url).then(res=>res.json()).then(data=> {
-            setItem(data.meals);
-            setShow(true);
-        })
-     },[url])
+  const [search, setSearch] = useState();
+  const [show, setShow] = useState(false);
+  const [item, setItem] = useState("");
+  const [url, setUrl] = useState(
+    "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
+  );
 
-     const searchRecipe=(evt)=>{
-         setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-    }
-    // const setIndex=(alpha)=>{
-    //     setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`);
-    // }
-return (
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setItem(data.meals);
+        setShow(true);
+      });
+  }, [url]);
+
+  // eslint-disable-next-line no-unused-vars
+  const searchRecipe = (evt) => {
+    setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+  };
+  // const setIndex=(alpha)=>{
+  //     setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`);
+  // }
+  return (
     <>
-        <div className="main">
-            <div className="heading">
-                <GiFoodTruck color="white"size="5rem"/>
-                <h1>Search Your Food Recipe</h1>
-            </div>
-            <div className="searchBox">
-                <input type="search" className="search-bar" onChange={e=> setSearch(e.target.value)} onKeyPress={searchRecipe}/>
-            </div>
-            <div className="container">
-                {
-                    show ?<MealItem data={item} /> :"Not Found"
-                
-                }
-            </div>
-            {/* <div className="indexContainer">
+      <div className="main">
+        <div className="heading">
+          <GiFoodTruck color="white" size="5rem" />
+          <h1>Search Your Food Recipe</h1>
+        </div>
+        <div className="searchBox">
+          <input
+            type="search"
+            className="search-bar"
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyPress={searchRecipe}
+          />
+        </div>
+        <div className="container">
+          {show ? <MealItem data={item} /> : "Not Found"}
+        </div>
+        {/* <div className="indexContainer">
                  <ReacipeIndex alphaIndex={(alpha)=>setIndex(alpha)}/>
             </div> */}
-            
-        </div>
+      </div>
     </>
-)
-}
+  );
+};
 export default Meal;
--- src/Components/MealItem.jsx
+++ src/Components/MealItem.jsx
@@ -1,22 +1,29 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
-const MealItem=({data})=>{
-    console.log(data);
-    let navigate = useNavigate();
-    return(
-        <>  
-            {
-                (!data) ?"Not Found": data.map(item=>{
-                    return(
-                    <div className="card" key={item.idMeal} onClick={()=>navigate(`/${item.idMeal}`)}>
-                        <img src={item.strMealThumb} alt="" />
-                        <h3>{item.strMeal}</h3>
-                    </div>
-                    )
-                })
-            }
-            
-        </>
-    )
-}
+// eslint-disable-next-line react/prop-types
+const MealItem = ({ data }) => {
+  console.log(data);
+  let navigate = useNavigate();
+  return (
+    <>
+      {
+        // eslint-disable-next-line react/prop-types
+        !data
+          ? "Not Found"
+          : // eslint-disable-next-line react/prop-types
+            data.map((item) => {
+              return (
+                <div
+                  className="card"
+                  key={item.idMeal}
+                  onClick={() => navigate(`/${item.idMeal}`)}
+                >
+                  <img src={item.strMealThumb} alt="" />
+                  <h3>{item.strMeal}</h3>
+                </div>
+              );
+            })
+      }
+    </>
+  );
+};
 export default MealItem;
